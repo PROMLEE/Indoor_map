010c20535645981614f72c13253c707d7cb7831d
Donghoon Lee <donghoon099@naver.com>
2023-12-05 01:01:16 +0900

    dd

--- scripts/edit_json.py
+++ scripts/edit_json.py
@@ -142,6 +142,6 @@ def update_move_down(data, id):
 # newdata = [{"id": 1, "caption": "ddddd"}]
 # update_caption(data, newdata)
 # print(data)
-# building_name = "CAU310_B4"
-# create_mask(building_name)
+building_name = "CAU310_01"
+create_mask(building_name)
 # update_captiontoint()
