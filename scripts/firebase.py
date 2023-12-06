b327feec0ceb128671f3875b9b6e36a6d6ff242b
Donghoon Lee <donghoon099@naver.com>
2023-12-06 18:55:47 +0900

    firebase comp

--- scripts/firebase.py
+++ scripts/firebase.py
@@ -1,38 +1,28 @@
-import json
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-import timeit
+
 
 cred = credentials.Certificate(
     "scripts\capston-design7-firebase-adminsdk-p4k7t-1579383ce6.json"
 )
 firebase_admin.initialize_app(cred)
 
-db = firestore.client()
-collectionname = "buildings"
-floor = "B6"
 
-
-def set(item):
-    docs = db.collection("buildings").stream()
-    for doc in docs:
-        print(doc.id, doc.to_dict())
-
-    return (
-        db.collection(collectionname)
-        .document("42485677")
-        .collection("stores")
-        .document(f"42485677_{floor}")
-        .set(item)
-    )
-
-
-def get(buildingname):
+def put_firebase(filename, item):
+    db = firestore.client()
+    buildingname = filename[:-3]
+    floor = filename[-2:]
     docs = db.collection("buildings").stream()
     for doc in docs:
         if doc.to_dict()["BuildingName"] == buildingname:
-            print(doc.id)
+            return (
+                db.collection("buildings")
+                .document(doc.id)
+                .collection("stores")
+                .document(f"{doc.id}_{floor}")
+                .set(item)
+            )
 
 
 # f = open(f"result\CAU310\data\CAU310_{floor}.json")
@@ -46,4 +36,4 @@ def get(buildingname):
 #     dict[str(item["id"])] = item["caption"]
 # print(dict)
 # set(dict)
-print(get("CAU310"))
+# get("CAU310")
